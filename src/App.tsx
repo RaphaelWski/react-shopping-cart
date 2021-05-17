b7f740b865de7773fa0386b6e1d4ec2d726ee37a
Raph <rwojciechowski@alliancecentre.cerfrance.fr>
2021-05-17 19:32:24 +0200

    tring to do some stuff

--- src/App.tsx
+++ src/App.tsx
@@ -39,6 +39,15 @@ type State = {
   },
 }
 
+enum TaxRate {
+  Twenty = 20,
+  Ten = 10,
+  Five = 5.5,
+  Two = 2.1,
+  Nineteen = 19.6,
+  Seven = 7
+}
+
 export default class App extends Component<Props, State> {
   constructor(props: Props) {
     super(props)
@@ -77,38 +86,6 @@ export default class App extends Component<Props, State> {
         totalAmountIncludingTaxes: 24,
       },
     }
-  }
-
-  addToCart = (item: Product) => {
-    let { cart } = this.state
-    let cartProduct : CartProduct;
-
-    const product = cart.products.find(product => product.productId === item.id)
-    if(product && product.quantity) {
-      cartProduct = { productId: item.id, quantity: product.quantity+1 }
-      this.updateCartPrice(item);
-
-      cart = {
-        ...cart,
-        products: [
-          ...cart.products.filter(product => product.productId !== item.id), // Attention à l'ordre
-          cartProduct
-        ]
-      }
-    } else {
-      cartProduct = { productId: item.id, quantity: 1 }
-      this.updateCartPrice(item);
-
-      cart = {
-        ...cart,
-        products: [
-          ...cart.products,
-          cartProduct
-        ]
-      }
-    }
-    
-    this.setState({ cart })
   }
 
   updateCartPrice = (product: Product) => {
@@ -153,6 +130,68 @@ export default class App extends Component<Props, State> {
     return total
   }
 
+  addToCart = (item: Product) => {
+    let { cart } = this.state
+    let cartProduct : CartProduct;
+
+    const product = cart.products.find(product => product.productId === item.id)
+    if(product && product.quantity) {
+      cartProduct = { productId: item.id, quantity: product.quantity+1 }
+      // this.updateCartPrice(item); /** ne mets pas à jour le state */
+
+      /** START */
+      // [Montant HT] = [Montant TTC] / (1 + ([Taux TVA] / 100))
+      let valueHT = item.price / (1 + (item.tax / 100));
+      let value = item.price - valueHT;
+      let tax = { name: item.tax, value: this.formatNumber(value) };
+      console.log(tax);
+      let totalAmountIncludingTaxes = this.updateTotalAmount();
+      console.log(totalAmountIncludingTaxes);
+      /** END */
+
+      cart = {
+        // ...cart,
+        products: [
+          ...cart.products.filter(product => product.productId !== item.id), // Attention à l'ordre
+          cartProduct
+        ],
+        taxes: [
+          ...cart.taxes,
+          tax
+        ],
+        totalAmountIncludingTaxes: totalAmountIncludingTaxes
+      }
+    } else {
+      cartProduct = { productId: item.id, quantity: 1 }
+      // this.updateCartPrice(item); /** ne mets pas à jour le state */
+
+      /** START */
+      // [Montant HT] = [Montant TTC] / (1 + ([Taux TVA] / 100))
+      let valueHT = item.price / (1 + (item.tax / 100));
+      let value = item.price - valueHT;
+      let tax = { name: item.tax, value: this.formatNumber(value) };
+      console.log(tax);
+      let totalAmountIncludingTaxes = this.updateTotalAmount();
+      console.log(totalAmountIncludingTaxes);
+      /** END */
+
+      cart = {
+        // ...cart,
+        products: [
+          ...cart.products.filter(product => product.productId !== item.id), // Attention à l'ordre
+          cartProduct
+        ],
+        taxes: [
+          ...cart.taxes,
+          tax
+        ],
+        totalAmountIncludingTaxes: totalAmountIncludingTaxes
+      }
+    }
+    
+    this.setState({ cart })
+  }
+
   removeFromCart = (item: Product) => {
     let { cart } = this.state
     let cartProduct : CartProduct;
@@ -160,25 +199,51 @@ export default class App extends Component<Props, State> {
     const product = cart.products.find(product => product.productId === item.id)
     if(product && product.quantity) {
       if(product.quantity == 1) {
+        /** START */
+        // [Montant HT] = [Montant TTC] / (1 + ([Taux TVA] / 100))
+        let valueHT = item.price / (1 + (item.tax / 100));
+        let value = item.price - valueHT;
+        let tax = { name: item.tax, value: this.formatNumber(value) };
+        console.log(tax);
+        // let totalAmountIncludingTaxes = this.updateTotalAmount();
+        let totalAmountIncludingTaxes: number = 0;
+          /** START */
+          let { data } = this.state;
+          cart.products.map( productCart => {
+            const productData = data.products.find(product => product.id === productCart.productId)
+            if(productData && productData.price) {
+              totalAmountIncludingTaxes += productCart.quantity * productData.price
+            }
+          });
+          /** END */
+        console.log(totalAmountIncludingTaxes);
+        console.log(typeof cart);
+        /** END */
+
+        cart = {
+          products: [
+            ...cart.products.filter(product => product.productId !== item.id)  // Attention à l'ordre
+          ],
+          taxes: [
+            ...cart.taxes,
+            tax
+          ],
+          totalAmountIncludingTaxes: totalAmountIncludingTaxes
+        }
+
+      } else {
+        cartProduct = { productId: item.id, quantity: product.quantity-1 }
+        this.updateCartPrice(item);
+  
         cart = {
           ...cart,
           products: [
-            ...cart.products.filter(product => product.productId !== item.id),  // Attention à l'ordre
+            ...cart.products.filter(product => product.productId !== item.id), // Attention à l'ordre
+            cartProduct
           ]
         }
-      } else {
-          cartProduct = { productId: item.id, quantity: product.quantity-1 }
-          this.updateCartPrice(item);
-    
-          cart = {
-            ...cart,
-            products: [
-              ...cart.products.filter(product => product.productId !== item.id), // Attention à l'ordre
-              cartProduct
-            ]
-          }
-        }
-      }    
+      }
+    } 
     this.setState({ cart })
   }
   
@@ -195,12 +260,67 @@ export default class App extends Component<Props, State> {
     this.setState({ cart });
   }
 
+  checkoutCart = () => {
+    console.log('Checkout Cart action');
+  }
+
   handleChange = () => {
     this.setState(this.state);
   }
 
+  renderItem = (item: Product) => {
+    const { id, name, description } = item
+    const media = <Avatar customer={true} size="large" name={name} />;
+    const shortcutActions = [
+      {
+        content: `Add`,
+        onAction: () => {
+          this.addToCart(item);
+        }
+      },
+      {
+        content: `Remove`,
+        onAction: () => {
+          this.removeFromCart(item);
+        }
+      },
+    ]
+    return (
+      // @ts-ignore
+      <ResourceList.Item
+        id={id.toString()}
+        media={media}
+        accessibilityLabel={`View details for ${name}`}
+        shortcutActions={shortcutActions}
+        persistActions={true}
+        onClick={ () => {
+          // Gestion du panier (changement du state et calcul du total)
+          console.log(item)
+          this.handleChange
+        }}
+      >
+        <h3>
+          <TextStyle variation="strong">{name}</TextStyle>
+        </h3>
+        <div>{description}</div>
+      </ResourceList.Item>
+    )
+  }
+
   render() {
     const { data, cart } = this.state
+    
+    /* Cart */
+    const cancelCartAction =       {
+      content: 'Cancel cart',
+      onAction: () => this.cancelCart()
+    }
+    
+    const checkoutCartAction = {
+      content: 'Pay',
+      onAction: () => this.checkoutCart()
+    };
+    
     return (
       <Page title="React Shopping Cart">
         <Layout>
@@ -209,47 +329,15 @@ export default class App extends Component<Props, State> {
               <ResourceList
                 resourceName={{ singular: 'customer', plural: 'customers' }}
                 items={data.products}
-                renderItem={item => {
-                  const { id, name, description } = item
-                  const media = (
-                    <Avatar customer={true} size="medium" name={name} />
-                  )
-                  const shortcutActions = [
-                    {
-                      content: `Add ${item.name}`,
-                      onAction: () => {
-                        this.addToCart(item);
-                      }
-                    },
-                  ]
-                  return (
-                    // @ts-ignore
-                    <ResourceList.Item
-                      id={id}
-                      media={media}
-                      accessibilityLabel={`View details for ${name}`}
-                      shortcutActions={shortcutActions}
-                      persistActions={true}
-                      onClick={ () => {
-                        console.log(item)
-                        this.handleChange
-                      }}
-                    >
-                      <h3>
-                        <TextStyle variation="strong">{name}</TextStyle>
-                      </h3>
-                      <div>{description}</div>
-                    </ResourceList.Item>
-                  )
-                }}
+                renderItem={this.renderItem}
               />
             </Card>
           </Layout.Section>
           <Layout.Section secondary>
             <Card
               title="Basket"
-              secondaryFooterAction={{ content: 'Cancel cart', onAction: () => { this.cancelCart();}, }}
-              primaryFooterAction={{ content: 'Pay', }}
+              secondaryFooterAction={cancelCartAction}
+              primaryFooterAction={checkoutCartAction}
             >
               <Card.Section title="Items">
                 <List>
@@ -263,13 +351,27 @@ export default class App extends Component<Props, State> {
               </Card.Section>
               <Card.Section title="Totals">
                 <List>
-                  {cart.taxes.map(tax => (
-                    <List.Item>
-                      TVA {tax.name}% : {tax.value.toFixed(2)}€
-                    </List.Item>
-                  ))}
+                  {cart.taxes.map(tax => {
+                      let sumTwenty, sumTen, sumFive, sumTwo, sumNineteen, sumSeven;
+                      if(tax.name == TaxRate.Twenty) sumTwenty = cart.taxes.reduce((total, tax) => this.formatNumber(tax.value) + total,0)
+                      if(tax.name == TaxRate.Ten) sumTen = cart.taxes.reduce((total, tax) => this.formatNumber(tax.value) + total,0)
+                      if(tax.name == TaxRate.Five) sumFive = cart.taxes.reduce((total, tax) => this.formatNumber(tax.value) + total,0)
+                      if(tax.name == TaxRate.Two) sumTwo = cart.taxes.reduce((total, tax) => this.formatNumber(tax.value) + total,0)
+                      if(tax.name == TaxRate.Nineteen) sumNineteen =cart.taxes.reduce((total, tax) => this.formatNumber(tax.value) + total,0)
+                      if(tax.name == TaxRate.Seven) sumSeven = cart.taxes.reduce((total, tax) => this.formatNumber(tax.value) + total,0)
+
+                      // if(sumTwenty) return <List.Item>TVA {TaxRate.Twenty}% : {this.formatNumber(sumTwenty)}€</List.Item>
+                      // if(sumTen) return <List.Item>TVA {TaxRate.Ten}% : {this.formatNumber(sumTen)}€</List.Item>
+                      // if(sumFive) return <List.Item>TVA {TaxRate.Five}% : {this.formatNumber(sumFive)}€</List.Item>
+                      // if(sumTwo) return <List.Item>TVA {TaxRate.Two}% : {this.formatNumber(sumTwo)}€</List.Item>
+                      // if(sumNineteen) return <List.Item>TVA {TaxRate.Nineteen}% : {this.formatNumber(sumNineteen)}€</List.Item>
+                      // if(sumSeven) return <List.Item>TVA {TaxRate.Seven}% : {this.formatNumber(sumSeven)}€</List.Item>
+
+                      /** TODO: Utilisation des props pour update le composant */
+                    })
+                  }
                   <List.Item>
-                    {cart.totalAmountIncludingTaxes.toFixed(2)}€ TTC
+                    {this.formatNumber(cart.totalAmountIncludingTaxes)}€ TTC
                   </List.Item>
                 </List>
               </Card.Section>
